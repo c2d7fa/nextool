fae6ee7b2a8806bd99eb136f50b2400467e69f53
Jonas Hvid <mail@johv.dk>
2022-03-25 12:15:37 +0100

    Add screenshot and script to take screenshot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
     $ yarn parcel index.html
     $ yarn jest --watch-all --verbose
+
+![Screenshot](/screenshot.png?raw=true)
---
+++ screenshot.rb
@@ -0,0 +1,15 @@
+require "selenium-webdriver"
+
+options = Selenium::WebDriver::Chrome::Options.new(args: ["--force-device-scale-factor=1"])
+driver = Selenium::WebDriver.for :chrome, options: options
+wait = Selenium::WebDriver::Wait.new(timeout: 20)
+
+driver.get "http://localhost:1234/"
+
+driver.find_element(:xpath, "//*[contains(@class, 'titleColumn')]").click
+main = driver.find_element(:xpath, "//*[@id = 'root']")
+driver.execute_script("arguments[0].style.maxWidth = '1000px'", main)
+driver.execute_script("arguments[0].style.maxHeight = '450px'", main)
+driver.execute_script("arguments[0].style.minHeight = 'auto'", main)
+main.save_screenshot("screenshot.png")
+
