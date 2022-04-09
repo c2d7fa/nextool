23a3591872d98852c0b4d3767f66f989c1b4cd55
Jonas Hvid <mail@johv.dk>
2022-04-09 22:41:38 +0200

    Update screenshot to show 'Today' badge and multiple filters

--- screenshot.rb
+++ screenshot.rb
@@ -6,7 +6,11 @@ wait = Selenium::WebDriver::Wait.new(timeout: 20)
 
 driver.get "http://localhost:1234/"
 
-driver.find_element(:xpath, "//*[contains(@class, 'titleColumn')]").click
+date = Time.now.strftime("%m/%d/%Y")
+
+driver.find_element(:xpath, "//*[contains(text(), 'Task 1')]").click
+driver.find_element(:xpath, "//*[contains(text(), 'Planned')]/following::input[@type = 'date']").send_keys(date)
+
 driver.find_element(:xpath, "//*[contains(text(), 'Actions')]").click
 main = driver.find_element(:xpath, "//*[@id = 'root']")
 driver.execute_script("arguments[0].style.maxWidth = '1000px'", main)
