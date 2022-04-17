c0e8fd534bba502c285b1fa493f9021014ec66f5
Jonas Hvid <mail@johv.dk>
2022-04-18 00:25:28 +0200

    Require full tree node to be passed to `anyDescendant`
    Since we were doing this anyway.

--- indented-list.ts
+++ indented-list.ts
@@ -321,11 +321,9 @@ export function anyAncestor<D>(tree: Tree<D>, query: Handle, predicate: (ancesto
 
 export function anyDescendant<D>(
   tree: Tree<D>,
-  query: Handle,
+  node: TreeNode<D>,
   predicate: (descendant: TreeNode<D>) => boolean,
 ): boolean {
-  const node = findNode(tree, query);
-  if (!node) return false;
   if (predicate(node)) return true;
   return node.children.some((child) => anyDescendant(tree, child, predicate));
 }
