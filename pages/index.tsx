cd69dd798427ac4f8f6ce8de6a0feb904118ed6b
Jonas Hvid <mail@johv.dk>
2022-04-27 01:16:55 +0200

    Add a little more information to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,18 @@ export default function Index() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className={styles.heading}>Nextool</h1>
         <p>
-          See <a href="https://github.com/c2d7fa/nextool">the GitHub repository</a> for now.
+          Nextool is a GTD-oriented task manager with support for nested tasks and a focus on finding actionable
+          tasks. It's a work-in-progress that's currently usable but not yet useful.
         </p>
         <p>
-          <Link href="/download">Download</Link>
+          For now, see <a href="https://github.com/c2d7fa/nextool">the GitHub repository</a> for more information.
         </p>
+        <p>
+          <Link href="/download">Download for Linux, macOS or Windows.</Link>
+        </p>
+        <img src="https://raw.githubusercontent.com/c2d7fa/nextool/main/screenshot.png" />
       </main>
 
       <script data-goatcounter="https://nextool.goatcounter.com/count" async src="//gc.zgo.at/count.js" />
