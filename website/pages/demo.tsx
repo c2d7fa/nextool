012fc788f7ba29704f482a6b92bdc21c0e3939de
Jonas Hvid <mail@johv.dk>
2022-05-12 15:20:34 +0200

    Add an actual instance of the application to `/demo`
    This uses a somewhat hacky setup similar to the one used for the
    Electron build (see README for more information).

--- website/pages/demo.tsx
+++ website/pages/demo.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 
+import * as Nextool from "nextool";
+import "nextool/dist/index.css";
+
 export default function Index() {
   return (
     <>
@@ -7,7 +10,38 @@ export default function Index() {
         <title>Nextool</title>
       </Head>
 
-      <main>Hi. This page will contain a demo soon :)</main>
+      <Nextool.Main
+        platform={{
+          async readLocalStorage() {
+            return localStorage.getItem("tasks");
+          },
+          async saveLocalStorage(value) {
+            return localStorage.setItem("tasks", value);
+          },
+          async fileDownload({name, contents}) {
+            const downloadLinkElement = document.createElement("a");
+            downloadLinkElement.setAttribute("href", URL.createObjectURL(new Blob([contents])));
+            downloadLinkElement.setAttribute("download", name);
+            downloadLinkElement.click();
+          },
+          async fileUpload() {
+            return new Promise((resolve, reject) => {
+              const input = document.createElement("input");
+              input.type = "file";
+              input.onchange = (ev) => {
+                const file = (ev.target as any).files[0];
+                const reader = new FileReader();
+                reader.onload = (ev) => {
+                  const contents = ev.target.result;
+                  resolve({name: file.name, contents});
+                };
+                reader.readAsText(file);
+              };
+              input.click();
+            });
+          },
+        }}
+      />
 
       <script data-goatcounter="https://nextool.goatcounter.com/count" async src="//gc.zgo.at/count.js" />
     </>
