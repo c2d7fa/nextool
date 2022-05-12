8828934d8b1d21cc446e77aafb2b6d453fbcba6a
Jonas Hvid <mail@johv.dk>
2022-05-12 15:12:45 +0200

    Add placeholder for `/demo`
    This is mostly to test whether the Vercel deployment works after switching from a
    separate repository.

---
+++ website/pages/demo.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Nextool</title>
+      </Head>
+
+      <main>Hi. This page will contain a demo soon :)</main>
+
+      <script data-goatcounter="https://nextool.goatcounter.com/count" async src="//gc.zgo.at/count.js" />
+    </>
+  );
+}
