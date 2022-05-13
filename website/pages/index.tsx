74444f6cfd358d9055a35a05bc1488b54f254109
Jonas Hvid <mail@johv.dk>
2022-05-13 14:18:48 +0200

    Replace screenshot with interactive demo

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import {GtdSteps} from "../lib/gtd-steps";
 import styles from "../styles/index.module.scss";
 
+import * as Nextool from "nextool";
+import "nextool/dist/index.css";
+
 export default function Index() {
   return (
     <>
@@ -16,7 +19,9 @@ export default function Index() {
           Nextool is a GTD-oriented task manager with support for nested tasks and a focus on finding actionable
           tasks. It's a work-in-progress that's currently usable but not yet useful.
         </p>
-        <img src="https://raw.githubusercontent.com/c2d7fa/nextool/main/screenshot.png" />
+        <div className={styles.demoContainer}>
+          <Nextool.SmallDemo />
+        </div>
         <p>
           <Link href="/download">Download for Linux, macOS or Windows.</Link> For now, see{" "}
           <a href="https://github.com/c2d7fa/nextool">the GitHub repository</a> for more information.
