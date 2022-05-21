1f9a2ba3c0940d325d3b6eff1140a05daaa324e1
Jonas Hvid <mail@johv.dk>
2022-05-21 23:17:52 +0200

    Refactor code for matching subtask filters

--- app/tasks.ts
+++ app/tasks.ts
@@ -229,29 +229,17 @@ function taskProject(state: CommonState, task: Task): null | {id: string} {
 }
 
 function doesSubtaskMatch(state: CommonState, task: Task): boolean {
-  const pausedSubtaskFilter = state.subtaskFilters.find((filter) => filter.id === "paused")?.state ?? "neutral";
-  if (pausedSubtaskFilter !== "neutral") {
-    if (
-      pausedSubtaskFilter === "include" &&
-      !IndentedList.anyDescendant(state.tasks, task, (subtask) => isPaused(state, subtask))
-    )
-      return false;
-    else if (pausedSubtaskFilter === "exclude" && isPaused(state, task)) return false;
+  function excludedBySubtaskFilter(filter: SubtaskFilter["id"], matches: (subtask: Task) => boolean): boolean {
+    const filterState = state.subtaskFilters.find((f) => f.id === filter)?.state ?? "neutral";
+    if (filterState === "include")
+      return !IndentedList.anyDescendant(state.tasks, task, (subtask) => matches(subtask));
+    if (filterState === "exclude")
+      return !IndentedList.anyDescendant(state.tasks, task, (subtask) => !matches(subtask));
+    return false;
   }
 
-  const doneSubtaskFilter = state.subtaskFilters.find((filter) => filter.id === "done")?.state ?? "neutral";
-  if (doneSubtaskFilter !== "neutral") {
-    if (
-      doneSubtaskFilter === "include" &&
-      !IndentedList.anyDescendant(state.tasks, task, (subtask) => isDone(subtask))
-    )
-      return false;
-    else if (
-      doneSubtaskFilter === "exclude" &&
-      !IndentedList.anyDescendant(state.tasks, task, (subtask) => !isDone(subtask))
-    )
-      return false;
-  }
+  if (excludedBySubtaskFilter("done", (subtask) => isDone(subtask))) return false;
+  if (excludedBySubtaskFilter("paused", (subtask) => isPaused(state, subtask))) return false;
 
   if (isArchived(state, task) && state.filter !== "archive") return false;
   if (state.filter === "stalled" || state.filter === "ready")
